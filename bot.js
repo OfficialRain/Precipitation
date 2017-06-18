e8e3555a95d133d30de6436b50209d2a2b29c53d
OfficialRain <ryanf0709@gmail.com>
2017-06-17 23:06:12 -0700

    Added basic fundamentals

--- bot.js
+++ bot.js
@@ -1,1 +1,24 @@
+/****************************************
+ * 
+ *   Precipitation/ShiftBot: Bot for Moderation under many Discord Servers
+ * 
+ * *************************************/
 
+const Discord = require('discord.js');
+const fs = require('fs');
+const client = new Discord.Client();
+
+client.login ("yEaH-No-tokEn-4uhaha-c543453464334634635756746rhfbers")
+
+var spamModeration = {};
+var capsModeration = {};
+
+function preventSpam(message)
+{
+spamModeration[message.guild.id]
+}
+
+function preventCaps(message)
+{
+capsModeration[message.guild.id]
+}
