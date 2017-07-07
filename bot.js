4b7b715b4e183ef1c78bfdb61b8b86813ed4806d
OfficialRain <ryanf0709@gmail.com>
2017-07-06 22:57:43 -0700

    Logs Console when ready
    also starting to get the game name to work

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 /****************************************
  * 
- *   Precipitation/ShiftBot: Bot for Moderation under many Discord Servers
+ *   Precipitation: Bot for Moderation under many Discord Servers
+ *   2017, Rain/Solexia
  * 
  * *************************************/
 
@@ -8,10 +9,31 @@ const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
 
-client.login ("yEaH-No-tokEn-4uhaha-c543453464334634635756746rhfbers")
-
 var spamModeration = {};
 var capsModeration = {};
+
+   client.on('ready', () => {
+       console.log("Precipitation is now ready! It works!");
+    client.setInterval(setGame, 300000);
+    setGame();
+   });
+
+    function setGame() {
+  var presence = {};
+  presence.game = {};
+  presence.status = "online";
+  presence.afk = false;
+
+  switch (Math.floor(Math.random() * 1000) % 35) {
+      case 0:
+       presence.game.name = "why is this a thing";
+       break;
+      case 1:
+        presence.game.name = "getting prepared to fight AstralMod and JXBot";
+        break;
+      }
+      client.user.setPresence(presence);
+  }
 
 function preventSpam(message)
 {
